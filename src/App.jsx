3767106290be945cb5fd57d34835d22a6d90b4d5
Hrithik <hrithik.kumar0210@gmail.com>
2024-07-23 21:17:10 +0530

    Material ui project starting

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Hello world</div>
+
+      <Button variant="text">Click</Button>
+    </>
+  );
+}
+
+export default App;
